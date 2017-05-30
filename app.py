24b59925bc025860d3b88c1a0ff5501c6d79d881
Bajda Daria <dabj@jcommerce.pl>
2017-05-30 17:58:43 +0200

    Authentication test 2

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
-from flask import Flask
-from datetime import datetime
+from flask import Flask, request, Response
+import os
 
-SLACK_WEBHOOK_SECRET = os.environ('SLACK_WEBHOOK_SECRET')
-
-base_url = 'http://cgbsclaim%s01:18001/claims/overview'
+SLACK_WEBHOOK_SECRET = os.environ['SLACK_WEBHOOK_SECRET']
+BASE_URL = 'http://cgbsclaim%s01:18001/claims/overview'
 
 app = Flask(__name__)
 
 def get_claims_url(env): 
-    return base_url % env
+    return BASE_URL % env
 
-def is_authenticated(request): 
-        return request.form.get('token') == SLACK_WEBHOOK_SECRET
+def is_authenticated():
+    return request.form.get('token') == SLACK_WEBHOOK_SECRET
 
 @app.route('/')
 def homepage():
@@ -19,13 +18,17 @@ def homepage():
 
 @app.route('/dev', methods=['POST'])
 def get_dev_env():
-    if is_authenticated(request):
-        return get_claims_url('dev')
+    if is_authenticated():
+        return get_claims_url('qa')
+    else:
+        return Response(), 401
 
 @app.route('/qa', methods=['POST'])
 def get_qa_env():
-    if is_authenticated(request):
+    if is_authenticated():
         return get_claims_url('qa')
+    else:
+        return Response(), 401
 
 @app.route('/test', methods=['POST'])
 def get_test_env():
