01f5d5b11d003ff80cdde3fae6d47a2b2c60bc69
Bajda Daria <dabj@jcommerce.pl>
2017-05-30 18:10:38 +0200

    Common auth

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask, request, Response
+from flask_httpauth import HTTPBasicAuth
 import os
 
 SLACK_WEBHOOK_SECRET = os.environ['SLACK_WEBHOOK_SECRET']
 BASE_URL = 'http://cgbsclaim%s01:18001/claims/overview'
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
 
 def get_claims_url(env): 
     return BASE_URL % env
 
+@auth.slack_token_required
 def is_authenticated():
     return request.form.get('token') == SLACK_WEBHOOK_SECRET
 
@@ -17,18 +20,14 @@ def homepage():
     return "sup?"
 
 @app.route('/dev', methods=['POST'])
+@auth.slack_token_required
 def get_dev_env():
-    if is_authenticated():
-        return get_claims_url('qa')
-    else:
-        return Response(), 401
+    return get_claims_url('dev')
 
 @app.route('/qa', methods=['POST'])
+@auth.slack_token_required
 def get_qa_env():
-    if is_authenticated():
-        return get_claims_url('qa')
-    else:
-        return Response(), 401
+    return get_claims_url('qa')
 
 @app.route('/test', methods=['POST'])
 def get_test_env():
