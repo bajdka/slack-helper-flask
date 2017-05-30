04e0274b48dfb4bc21f5bbe0f612465c8cfb686b
Bajda Daria <dabj@jcommerce.pl>
2017-05-30 18:26:43 +0200

    Authentication test 3

--- app.py
+++ app.py
@@ -1,31 +1,37 @@
 from flask import Flask, request, Response
-from flask_httpauth import HTTPBasicAuth
+from functools import wraps
 import os
 
 SLACK_WEBHOOK_SECRET = os.environ['SLACK_WEBHOOK_SECRET']
 BASE_URL = 'http://cgbsclaim%s01:18001/claims/overview'
 
 app = Flask(__name__)
-auth = HTTPBasicAuth()
 
 def get_claims_url(env): 
     return BASE_URL % env
 
-@auth.slack_token_required
-def is_authenticated():
+def check_auth():
     return request.form.get('token') == SLACK_WEBHOOK_SECRET
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if not check_auth():
+            return Response(), 401
+        return f(*args, **kwargs)
+    return decorated
 
 @app.route('/')
 def homepage():
     return "sup?"
 
 @app.route('/dev', methods=['POST'])
-@auth.slack_token_required
+@requires_auth
 def get_dev_env():
     return get_claims_url('dev')
 
 @app.route('/qa', methods=['POST'])
-@auth.slack_token_required
+@requires_auth
 def get_qa_env():
     return get_claims_url('qa')
 
