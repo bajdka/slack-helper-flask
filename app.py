9dd8a5f476f6c53ed11bf53dc69ed85fd5f69cd3
Bajda Daria <dabj@jcommerce.pl>
2017-05-30 19:43:24 +0200

    Wow refactor

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
 from functools import wraps
 import os
-from flask import Flask, request, Response
 import re
+from flask import Flask, request, Response
 
 SLACK_WEBHOOK_SECRET = os.environ['SLACK_WEBHOOK_SECRET']
-BASE_URL = 'http://cgbsclaim%s01:18001/claims/overview'
+CLAIMS_BASE_URL = 'http://cgbsclaim%s01:18001/claims/overview'
+CONTRACT_BASE_URL = 'http://cgbscontract%s01.cg.internal:18001/contracts/overview'
 JIRA_URL = 'https://jira.cargarantie.com/browse/BESTIMPL-%s'
+ENV_WARNING = 'You crazy? Provide correct environment (dev/qa/uat/test)'
+JIRA_WARNING = 'You crazy? Provide correct JIRA task number (e.g. 3242)'
+ENVIRONMENTS = ['dev', 'qa', 'test', 'uat']
 
 app = Flask(__name__)
 
 def get_claims_url(env):
-    return BASE_URL % env
+    return CLAIMS_BASE_URL % env
+
+def get_contract_url(env):
+    return CONTRACT_BASE_URL % env
 
 def get_jira_url(task_number):
     return JIRA_URL % task_number
@@ -27,31 +34,32 @@ def requires_auth(f):
         return f(*args, **kwargs)
     return decorated
 
-@app.route('/dev', methods=['POST'])
+@app.route('/claim', methods=['POST'])
 @requires_auth
-def get_dev_env():
-    return get_claims_url('dev')
+def get_claims_env():
+    env = request.form.get('text')
+    if env in ENVIRONMENTS:
+        return get_claims_url(env)
+    else:
+        return ENV_WARNING
 
-@app.route('/qa', methods=['POST'])
+@app.route('/contract', methods=['POST'])
 @requires_auth
-def get_qa_env():
-    return get_claims_url('qa')
-
-@app.route('/test', methods=['POST'])
-@requires_auth
-def get_test_env():
-    return get_claims_url('test')
+def get_contract_env():
+    env = request.form.get('text')
+    if env in ENVIRONMENTS:
+        return get_contract_url(env)
+    else:
+        return ENV_WARNING
 
 @app.route('/jira', methods=['POST'])
 @requires_auth
 def get_jira_link():
     jira_task_number = request.form.get('text')
-
-    # return "Text: %s, result: %s, result2: %s" % (jira_task_number, how, how2)
     if re.match("^[0-9]{4}$", jira_task_number):
         return get_jira_url(jira_task_number)
     else:
-        return 'You crazy? Provide correct JIRA task number (e.g. 3242)'
+        return JIRA_WARNING
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
