18c8a696bc618927a5af8aea20a19c063700389c
Bajda Daria <dabj@jcommerce.pl>
2017-05-30 18:55:10 +0200

    Refactor

--- app.py
+++ app.py
@@ -1,29 +1,25 @@
-from flask import Flask, request, Response
 from functools import wraps
 import os
+from flask import Flask, request, Response
 
 SLACK_WEBHOOK_SECRET = os.environ['SLACK_WEBHOOK_SECRET']
 BASE_URL = 'http://cgbsclaim%s01:18001/claims/overview'
 
 app = Flask(__name__)
 
-def get_claims_url(env): 
+def get_claims_url(env):
     return BASE_URL % env
 
-def check_auth():
+def is_authorized():
     return request.form.get('token') == SLACK_WEBHOOK_SECRET
 
 def requires_auth(f):
     @wraps(f)
     def decorated(*args, **kwargs):
-        if not check_auth():
+        if not is_authorized():
             return Response(), 401
         return f(*args, **kwargs)
     return decorated
-
-@app.route('/')
-def homepage():
-    return "sup?"
 
 @app.route('/dev', methods=['POST'])
 @requires_auth
@@ -36,6 +32,7 @@ def get_qa_env():
     return get_claims_url('qa')
 
 @app.route('/test', methods=['POST'])
+@requires_auth
 def get_test_env():
     return get_claims_url('test')
 
