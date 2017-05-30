b1d8626b1b8d68d1b72993854a57c10695b4c3dc
Bajda Daria <dabj@jcommerce.pl>
2017-05-30 17:09:08 +0200

    Endpoints added

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
 from flask import Flask
 from datetime import datetime
+
+base_url = 'http://cgbsclaim%s01:18001/claims/overview'
+
 app = Flask(__name__)
+
+def get_claims_url(env): 
+    return base_url % env
 
 @app.route('/')
 def get_url():
-    return "google.com"
+    return "sup?"
+
+@app.route('/dev')
+def get_dev_env():
+    return get_claims_url('dev')
+
+@app.route('/qa')
+def get_qa_env():
+    return get_claims_url('qa')
+
+@app.route('/test')
+def get_test_env():
+    return get_claims_url('test')
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
