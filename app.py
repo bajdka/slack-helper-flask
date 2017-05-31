ff6bc2a3786ef1946188e13128c185efb907aee5
Bajda Daria <dabj@jcommerce.pl>
2017-05-31 21:37:04 +0200

    Method fix

--- app.py
+++ app.py
@@ -94,9 +94,9 @@ def send_kudo():
     data = {
         "attachments": [
             {"color": "#439FE0",
-             "pretext": "Kudo poszło w świat! :party_parrot:",
-             "text": ":star2: %s :star2:\n*%s*" % (user, reason),
-             "mrkdwn_in":["text", "pretext"]}], "response_type":"in_channel"}
+             "text": "Kudo poszło w świat! :partyparrot:\n:star2: %s :star2:\n*%s*" % (user, reason),
+             "mrkdwn_in":["text"]}],
+        "response_type":"in_channel"}
 
     requests.post(url, data=json.dumps(data), headers=headers)
     return Response(), 200
