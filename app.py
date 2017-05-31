c970d9a00ca2b6389bfc80c00dce9f9abde7de6c
Bajda Daria <dabj@jcommerce.pl>
2017-05-31 21:53:19 +0200

    Link names

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ def send_kudo():
         "attachments": [
             {"color": "#439FE0",
              "text": "Nowe kudo! \n:star2: %s :star2:\n*%s*" % (user, reason),
-             "mrkdwn_in":["text"]}], "response_type":"in_channel"}
+             "mrkdwn_in":["text"]}], "response_type":"in_channel", "link_names":1}
 
     requests.post(url, data=json.dumps(data), headers=headers)
     return Response(), 200
